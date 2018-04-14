6c5fb7e5cdc78e3bd80e4646ce76b78aa320b28f
Luis Miguel López <luis_miguel@outlook.com>
2018-04-13 22:58:18 -0500

    Update 3lnn.h

--- src/3lnn.h
+++ src/3lnn.h
@@ -21,3 +21,42 @@ struct Node{
     int wcount;
     double weights[];
 };
+
+//Estructura que contiene el numero de conexiones por capa
+struct Layer{
+    int ncount;
+    Node nodes[];
+};
+
+//Estructura que contiene toda la red neuronal
+struct Network{
+    int inpNodeSize;
+    int inpuLayerSize;
+    int hiNodeSize;
+    int hiLayerSize;
+    int outNodeSize;
+    int outLayerSize;
+    double learningRate;
+    ActFctType hidLayerActType;
+    ActFctType outLayerActType;
+    Layer layers[];
+};
+
+/*Se crea la red que contiene tres capas: entrada, capa oculta y salida
+ inpCount = numero de conexiones en la entrada
+ hiCount = numero de conexiones en la capa oculta
+ outCount = numero de conexiones ne la salida */
+Network *createNetwork(int inpCount, int hidCount, int outCount);
+
+/* Se utiliza un vector donde esta hay informacion de las imagenes 
+   para la entrada de la red neuronal*/
+void feedInput(Network *nn, Vector *v);
+
+// Se hace el recorrido de la entrada a la capa oculta y de ahí a la saliad
+void feedForwardNetwork(Network *nn);
+
+//Se propaga el error de la salida a la capa oculta
+void backPropagateNetwork(Network *nn, int targetClassification);
+
+//Obtenemos el resultado de la red neuronal
+int getNetworkClassification(Network *nn);
