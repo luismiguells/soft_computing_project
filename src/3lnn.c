dcf9e2b518bd2234360cf7a0c70934d76ed7ba71
Luis Miguel López <luis_miguel@outlook.com>
2018-04-17 15:03:56 -0500

    Update 3lnn.c

--- src/3lnn.c
+++ src/3lnn.c
@@ -3,6 +3,8 @@
 #include "3lnn.h"
 #include "mnist.h"
 
+
+//Se busca la conexión en cada uno de los nodos
 Node *getNode(Layer *l, int nodeId){
     int nodeSize = sizeof(Node) + (l->nodes[0].wcount*sizeof(double));
     uint8_t *sbptr = (uint8_t*) l->nodes;
@@ -12,20 +14,24 @@ Node *getNode(Layer *l, int nodeId){
     return (Node*) sbptr;
 }
 
+//Se accedera a cada una de las capas
 Layer *getLayer(Network *nn, LayerType ltype){
     Layer *l;
 
     switch(ltype){
+        //Datos de entrada
         case INPUT:{
             l = nn->layers;
             break;
         }
+        //Se obtiene la entrada para la capa ocula
         case HIDDEN:{
             uint8_t *sbptr = (uint8_t*) nn->layers;
             sbptr += nn->inpLayerSize;
             l = (Layer*)sbptr;
             break;
         }
+        //Se obtiene la salida 
         default:{
             uint8_t *sbptr = (uint8_t*) nn->layers;
             sbptr += nn->inpLayerSize + nn->hidLayerSize;
@@ -36,17 +42,20 @@ Layer *getLayer(Network *nn, LayerType ltype){
 
     return l;
 }
-       
+
+//Funcion de la derivada de la funcion de activacin 
 double getActFctDerivative(Network *nn, LayerType ltype, double outVal){
     double dVal = 0;
     ActFctType actFct;
 
+    //Se aplica la funcion a la salida de la capa oculta y a la salida
     if(ltype == HIDDEN){
         actFct = nn->hidLayerActType;
     }else{
         actFct = nn->outLayerActType;
     }
 
+    //Derivada
     if(actFct == SIGMOID){
         dVal = outVal * (1 - outVal);
     }
@@ -54,6 +63,7 @@ double getActFctDerivative(Network *nn, LayerType ltype, double outVal){
     return dVal;
 }
 
+//Se actualizan los pesos segun el error 
 void updateNodeWeights(Network *nn, LayerType ltype, int id, double error){
     
     Layer *updateLayer = getLayer(nn, ltype);
@@ -61,6 +71,7 @@ void updateNodeWeights(Network *nn, LayerType ltype, int id, double error){
     
     Layer *prevLayer;
     int prevLayerNodeSize = 0;
+    //Se van actualizando de la salida a la entrada
     if (ltype == HIDDEN) {
         prevLayer = getLayer(nn, INPUT);
         prevLayerNodeSize = nn->inpNodeSize;
@@ -77,44 +88,41 @@ void updateNodeWeights(Network *nn, LayerType ltype, int id, double error){
         sbptr += prevLayerNodeSize;
     }
     
-    // update bias weight
+    //Se actualiza el bias del peso
     updateNode->bias += (nn->learningRate * 1 * error);
     
 }
 
-
-
-
-/**
- * @brief Back propagates network error to hidden layer
- * @param nn A pointer to the NN
- * @param targetClassification Correct classification (=label) of the input stream
- */
-
+//Backpropagation en la capa oculta
 void backPropagateHiddenLayer(Network *nn, int targetClassification){
     
+    //Seleccionamos el tipo de capa
     Layer *ol = getLayer(nn, OUTPUT);
     Layer *hl = getLayer(nn, HIDDEN);
     
-    for (int h=0;h<hl->ncount;h++){
+    for (int h = 0;h < hl->ncount;h++){
+        //Creamos la variable donde se almacenara las conexiones
         Node *hn = getNode(hl,h);
         
         double outputcellerrorsum = 0;
         
-        for (int o=0;o<ol->ncount;o++){
+        for (int o = 0; o < ol->ncount; o++){
             
             Node *on = getNode(ol,o);
             
-            int targetOutput = (o==targetClassification)?1:0;
+            int targetOutput = (o == targetClassification) ? 1 : 0;
             
+            //Calculo de las delta
             double errorDelta = targetOutput - on->output;
+            //Delta por la derivada
             double errorSignal = errorDelta * getActFctDerivative(nn, OUTPUT, on->output);
-            
+            //calculamos la suma de los errores
             outputcellerrorsum += errorSignal * on->weights[h];
         }
         
         double hiddenErrorSignal = outputcellerrorsum * getActFctDerivative(nn, HIDDEN, hn->output);
         
+        //Actualizamos los pesos
         updateNodeWeights(nn, HIDDEN, h, hiddenErrorSignal);
     }
     
@@ -123,40 +131,31 @@ void backPropagateHiddenLayer(Network *nn, int targetClassification){
 
 
 
-/**
- * @brief Back propagates network error in output layer
- * @param nn A pointer to the NN
- * @param targetClassification Correct classification (=label) of the input stream
- */
-
+//Backpropagation de la salida 
 void backPropagateOutputLayer(Network *nn, int targetClassification){
     
+    //Seleccionamos la capa 
     Layer *ol = getLayer(nn, OUTPUT);
     
-    for (int o=0;o<ol->ncount;o++){
+    for (int o = 0; o < ol->ncount; o++){
         
         Node *on = getNode(ol,o);
         
-        int targetOutput = (o==targetClassification)?1:0;
-        
+        //Se obtiene el error
+        int targetOutput = (o == targetClassification) ? 1 : 0;
+        //Se obtiene la delta
         double errorDelta = targetOutput - on->output;
+        //Se multiplica la delta por la derivada
         double errorSignal = errorDelta * getActFctDerivative(nn, OUTPUT, on->output);
         
+        //Se actualizan los pesos
         updateNodeWeights(nn, OUTPUT, o, errorSignal);
         
     }
     
 }
 
-
-
-
-/**
- * @brief Back propagates network error from output layer to hidden layer
- * @param nn A pointer to the NN
- * @param targetClassification Correct classification (=label) of the input stream
- */
-
+//Se propaga de la salida a la capa oculta de la capa oculta a la entrada
 void backPropagateNetwork(Network *nn, int targetClassification){
     
     backPropagateOutputLayer(nn, targetClassification);
@@ -165,29 +164,24 @@ void backPropagateNetwork(Network *nn, int targetClassification){
     
 }
 
-
-
-
-/**
- * @brief Performs an activiation function (as defined in the NN's defaults) to a specified node
- * @param nn A pointer to the NN
- * @param ltype Type of layer (INPUT, HIDDEN, OUTPUT)
- * @param id Sequential id of the node that is to be calculated
- */
-
+//Funcion de activiacion de cada una de las conexiones 
 void activateNode(Network *nn, LayerType ltype, int id){
     
+    //Obtenemos la capa
     Layer *l = getLayer(nn, ltype);
+    //Obtenemos la conexcion
     Node *n = getNode(l, id);
     
     ActFctType actFct;
     
+    //Se realiza la funcion de activacion
     if (ltype == HIDDEN){
         actFct = nn->hidLayerActType;
     }else{
         actFct = nn->outLayerActType;
     }
     
+    //Formula de la funcion de activacion
     if (actFct == SIGMOID){
         n->output = 1 / (1 + (exp((double)-n->output)));
     }
@@ -195,23 +189,17 @@ void activateNode(Network *nn, LayerType ltype, int id){
 }
 
 
-
-
-/**
- * @brief Calculates the output value of a specified node by multiplying all its weights with the previous layer's outputs
- * @param nn A pointer to the NN
- * @param ltype Type of layer (INPUT, HIDDEN, OUTPUT)
- * @param id Sequential id of the node that is to be calculated
- */
-
+//Se calcula la salida en de cada una de las capas multiplicando por el peso de cada una de las conexiones
 void calcNodeOutput(Network *nn, LayerType ltype, int id){
     
+    //Se obtienen la capa y las conexiones
     Layer *calcLayer = getLayer(nn, ltype);
     Node *calcNode = getNode(calcLayer, id);
     
     Layer *prevLayer;
     int prevLayerNodeSize = 0;
     
+    //Revisamos si es una de las capas 
     if (ltype == HIDDEN) {
         prevLayer = getLayer(nn, INPUT);
         prevLayerNodeSize = nn->inpNodeSize;
@@ -222,11 +210,11 @@ void calcNodeOutput(Network *nn, LayerType ltype, int id){
     
     uint8_t *sbptr = (uint8_t*) prevLayer->nodes;
     
-    // Start by adding the bias
+    //Se agrega el bias
     calcNode->output = calcNode->bias;
 
-    
-    for (int i=0; i<prevLayer->ncount;i++){
+    //Se realiza la multiplicacion por cada uno de los pesos
+    for (int i = 0; i < prevLayer->ncount; i++){
         Node *prevLayerNode = (Node*)sbptr;
         calcNode->output += prevLayerNode->output * calcNode->weights[i];
         sbptr += prevLayerNodeSize;
@@ -234,47 +222,25 @@ void calcNodeOutput(Network *nn, LayerType ltype, int id){
 
 }
 
-
-
-
-/**
- * @brief Calculates the output values of a given NN layer
- * @param nn A pointer to the NN
- * @param ltype Type of layer (INPUT, HIDDEN, OUTPUT)
- */
-
+//Se calcula la salida
 void calcLayer(Network *nn, LayerType ltype){
     Layer *l;
     l = getLayer(nn, ltype);
     
-    for (int i=0;i<l->ncount;i++){
+    for (int i = 0; i < l->ncount; i++){
         calcNodeOutput(nn, ltype, i);
         activateNode(nn,ltype,i);
     }
 }
 
-
-
-
-/**
- * @brief Feeds input layer values forward to hidden to output layer (calculation and activation fct)
- * @param nn A pointer to the NN
- */
-
+//Se realiza el calculo hacia delante 
 void feedForwardNetwork(Network *nn){
     calcLayer(nn, HIDDEN);
     calcLayer(nn, OUTPUT);
 }
 
 
-
-
-/**
- * @brief Feeds some Vector data into the INPUT layer of the NN
- * @param nn A pointer to the NN
- * @param v A pointer to a vector
- */
-
+//Se crea un vector el cual servira de entrada para la red
 void feedInput(Network *nn, Vector *v) {
     
     Layer *il;
@@ -283,42 +249,35 @@ void feedInput(Network *nn, Vector *v) {
     Node *iln;
     iln = il->nodes;
     
-    // Copy the vector content to the "output" field of the input layer nodes
-    for (int i=0; i<v->size;i++){
+    //Se copia la salida de la entrada
+    for (int i = 0; i < v->size; i++){
         iln->output = v->vals[i];
-        iln++;               // @warning This only works because inputNodeSize = sizeof(Node)
+        iln++;               
     }
     
 }
 
-
-
-
-/**
- * @details Creates an input layer and sets all weights to random values [0-1]
- * @param inpCount Number of nodes in the input layer
- */
-
+//Se crea una capa de entrada y los pesos de forma aleatoria [0-1]
 Layer *createInputLayer(int inpCount){
     
-    int inpNodeSize     = sizeof(Node);         // Input layer has 0 weights
+    int inpNodeSize     = sizeof(Node);         
     int inpLayerSize    = sizeof(Layer) + (inpCount * inpNodeSize);
     
     Layer *il = malloc(inpLayerSize);
     il->ncount = inpCount;
     
-    // Create a detault input layer node
+    //Se inicializan las variables
     Node iln;
     iln.bias = 0;
     iln.output = 0;
     iln.wcount = 0;
     
-    // Use a single byte pointer to fill in the network's content
+    //Se utiliza un puntero para ir obtinedo la informacion de la red
     uint8_t *sbptr = (uint8_t*) il->nodes;
     
-    // Copy the default input layer node x times
-    for (int i=0;i<il->ncount;i++){
-        memcpy(sbptr,&iln,inpNodeSize);
+    //Se copia la inicializacion n veces, donde n es el numero de entradas
+    for (int i = 0; i < il->ncount; i++){
+        memcpy(sbptr, &iln, inpNodeSize);
         sbptr += inpNodeSize;
     }
     
@@ -326,13 +285,7 @@ Layer *createInputLayer(int inpCount){
 }
 
 
-
-/**
- * @details Creates a layer and sets all weights to random values [0-1]
- * @param nodeCount Number of nodes
- * @param weightCount Number of weights per node
- */
-
+//Se crea una capa con los pesos de manera aleatoria [0-1]
 Layer *createLayer(int nodeCount, int weightCount){
     
     int nodeSize = sizeof(Node) + (weightCount * sizeof(double));
@@ -340,17 +293,19 @@ Layer *createLayer(int nodeCount, int weightCount){
     
     l->ncount = nodeCount;
     
-    // create a detault node
+    //Se inicializan las variables
     Node *dn = (Node*)malloc(sizeof(Node) + ((weightCount)*sizeof(double)));
     dn->bias = 0;
     dn->output = 0;
     dn->wcount = weightCount;
-    for (int o=0;o<weightCount;o++) dn->weights[o] = 0; // will be initialized later
+    for (int o = 0; o < weightCount; o++) dn->weights[o] = 0; 
     
     uint8_t *sbptr = (uint8_t*) l->nodes;     // single byte pointer
     
-    // copy the default cell to all cell positions in the layer
-    for (int i=0;i<nodeCount;i++) memcpy(sbptr+(i*nodeSize),dn,nodeSize);
+    //Se copian los valores
+    for (int i = 0; i < nodeCount; i++){
+        memcpy(sbptr+(i*nodeSize),dn,nodeSize);
+    }
     
     free(dn);
     
@@ -359,35 +314,27 @@ Layer *createLayer(int nodeCount, int weightCount){
 
 
 
-
-/**
- * @brief Initializes the NN by creating and copying INTPUT, HIDDEN, OUTPUT data structures into the NN's memory space
- * @param nn A pointer to the NN
- * @param inpCount Number of nodes in the INPUT layer
- * @param hidCount Number of nodes in the HIDDEN layer
- * @param outCount Number of nodes in the OUTPUT layer
- */
-
+//Se inicia la red neuronal copiando las estructuras de datos de entrada, capa oculata y salida
 void initNetwork(Network *nn, int inpCount, int hidCount, int outCount){
     
-    // Copy the input layer into the network's memory block and delete it
+    //Se copia la informacion de la entrada a la estructura de la red neuronal
     Layer *il = createInputLayer(inpCount);
-    memcpy(nn->layers,il,nn->inpLayerSize);
+    memcpy(nn->layers, il, nn->inpLayerSize);
     free(il);
     
-    // Move pointer to end of input layer = beginning of hidden layer
-    uint8_t *sbptr = (uint8_t*) nn->layers;     // single byte pointer
+    //Se mueve el puntero a la salida de la entrada que sera la entrada de la capa ocualta
+    uint8_t *sbptr = (uint8_t*) nn->layers;     
     sbptr += nn->inpLayerSize;
     
-    // Copy the hidden layer into the network's memory block and delete it
+    //Se copia la informacion de la capa oculta
     Layer *hl = createLayer(hidCount, inpCount);
     memcpy(sbptr,hl,nn->hidLayerSize);
     free(hl);
     
-    // Move pointer to end of hidden layer = beginning of output layer
+    //Se mueve el puntero a la salida de la capa oculta que sera la entrada de la salida de la red
     sbptr += nn->hidLayerSize;
     
-    // Copy the output layer into the network's memory block and delete it
+    //Se copia la informacion de la salida 
     Layer *ol = createLayer(outCount, hidCount);
     memcpy(sbptr,ol,nn->outLayerSize);
     free(ol);
@@ -395,55 +342,44 @@ void initNetwork(Network *nn, int inpCount, int hidCount, int outCount){
 }
 
 
-
-
-/**
- * @brief Sets the default network parameters (which can be overwritten/changed)
- * @param nn A pointer to the NN
- */
-
+//Inicializacion de los parametros de la red neuronal
 void setNetworkDefaults(Network *nn){
     
-    // Set deffault activation function types
+    //Funciones de activacion de cada una de las capas
     nn->hidLayerActType = SIGMOID;
     nn->outLayerActType = SIGMOID;
     
-    //nn->learningRate    = 0.004;    // TANH 78.0%
-    nn->learningRate    = 0.2;      // SIGMOID 91.5%
+    //Factor de aprendizaje
+    nn->learningRate    = 0.2;      
     
 }
 
 
-
-
-/**
- * @brief Initializes a layer's weights with random values
- * @param nn A pointer to the NN
- * @param ltype Defining what layer to initialize
- */
-
+//Se inicializan los pesos de manera random
 void initWeights(Network *nn, LayerType ltype){
     
     int nodeSize = 0;
     if (ltype == HIDDEN) nodeSize=nn->hidNodeSize;
                   else nodeSize=nn->outNodeSize;
     
+    //Selecciona la capa
     Layer *l = getLayer(nn, ltype);
     
     uint8_t *sbptr = (uint8_t*) l->nodes;
     
-    for (int o=0; o<l->ncount;o++){
+    //Se comienzan a asignar valores
+    for (int o = 0; o < l->ncount; o++){
     
         Node *n = (Node *)sbptr;
         
-        for (int i=0; i<n->wcount; i++){
-            n->weights[i] = 0.7*(rand()/(double)(RAND_MAX));
-            if (i%2) n->weights[i] = -n->weights[i];  // make half of the weights negative
+        for (int i = 0; i < n->wcount; i++){
+            n->weights[i] = 0.7*(rand()/(double)(RAND_MAX));//Funcion random
+            if (i%2) n->weights[i] = -n->weights[i];  //La mitad de los pesos son negativos
         }
         
-        // init bias weight
+        //Se inicializa el bias
         n->bias =  rand()/(double)(RAND_MAX);
-        if (o%2) n->bias = -n->bias;  // make half of the bias weights negative
+        if (o%2) n->bias = -n->bias;  //La mitad de los bias es negativo
         
         sbptr += nodeSize;
     }
@@ -451,26 +387,19 @@ void initWeights(Network *nn, LayerType ltype){
 }
 
 
-
-/**
- * @brief Creates a dynamically-sized, 3-layer (INTPUT, HIDDEN, OUTPUT) neural network
- * @param inpCount Number of nodes in the INPUT layer
- * @param hidCount Number of nodes in the HIDDEN layer
- * @param outCount Number of nodes in the OUTPUT layer
- */
-
+//Se crea la red neuronal
 Network *createNetwork(int inpCount, int hidCount, int outCount){
     
-    // Calculate size of INPUT Layer
-    int inpNodeSize     = sizeof(Node);         // Input layer has 0 weights
+    //Se calcula el tamaño de la entrada
+    int inpNodeSize     = sizeof(Node);        
     int inpLayerSize    = sizeof(Layer) + (inpCount * inpNodeSize);
     
-    // Calculate size of HIDDEN Layer
+    //Se calcula el tamaño de la capa oculta
     int hidWeightsCount = inpCount;
     int hidNodeSize     = sizeof(Node) + (hidWeightsCount * sizeof(double));
     int hidLayerSize    = sizeof(Layer) + (hidCount * hidNodeSize);
     
-    // Calculate size of OUTPUT Layer
+    //Se calcula el tamaño de la salida
     int outWeightsCount = hidCount;
     int outNodeSize     = sizeof(Node) + (outWeightsCount * sizeof(double));
     int outLayerSize    = sizeof(Layer) + (outCount * outNodeSize);
@@ -478,7 +407,7 @@ Network *createNetwork(int inpCount, int hidCount, int outCount){
     // Allocate memory block for the network
     Network *nn = (Network*)malloc(sizeof(Network) + inpLayerSize + hidLayerSize + outLayerSize);
     
-    // Set/remember byte sizes of each component of the network
+    //Se pone el tamaño de cada uno de los componentes de la red
     nn->inpNodeSize     = inpNodeSize;
     nn->inpLayerSize    = inpLayerSize;
     nn->hidNodeSize     = hidNodeSize;
@@ -486,38 +415,33 @@ Network *createNetwork(int inpCount, int hidCount, int outCount){
     nn->outNodeSize     = outNodeSize;
     nn->outLayerSize    = outLayerSize;
     
-    // Initialize the network by creating the INPUT, HIDDEN and OUTPUT layer inside of it
+    //Se crean cada una de las partes de la red
     initNetwork(nn, inpCount, hidCount, outCount);
     
-    // Setting defaults
+    //Se inicializan las variables
     setNetworkDefaults(nn);
     
-    // Init connection weights with random values
+    //Se inicializan los pesos de cada una de las capas
     initWeights(nn, HIDDEN);
     initWeights(nn, OUTPUT);
     
     return nn;
 }
 
-
-
-
-/**
- * @brief Returns the network's classification using the ID of teh node with the hightest output
- * @param nn A pointer to the NN
- */
-
+//Se obtiene la clasificacion tomando el valor maximo de la salida
 int getNetworkClassification(Network *nn){
     
+    //Se selecciona la salida 
     Layer *l = getLayer(nn, OUTPUT);
     
     double maxOut = 0;
     int maxInd = 0;
     
-    for (int i=0; i<l->ncount; i++){
+    //Se recorre para encontrar el valor
+    for (int i = 0; i < l->ncount; i++){
         
         Node *on = getNode(l,i);
-
+        //Se verifiva el valor maximo
         if (on->output > maxOut){
             maxOut = on->output;
             maxInd = i;
